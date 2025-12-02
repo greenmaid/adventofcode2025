8fdd42791ba7d4d716faf9a984350404e0721527
Mederic de Verdilhac <mederic.deverdilhac@orange.com>
2025-12-02 09:38:00 +0100

    Tooling

---
+++ src/dayxx/mod.rs
@@ -0,0 +1,19 @@
+use crate::tools;
+
+const CURRENT_DAY : &str = "dayxx";
+
+fn get_input_lines() -> Vec<String> {
+    tools::read_lines(CURRENT_DAY)
+}
+
+pub fn solve1() -> ( i32, i32) {
+    let input_lines = get_input_lines();
+    let result1 = 0;
+    return result1;
+}
+
+pub fn solve2() -> ( i32, i32) {
+    let input_lines = get_input_lines();
+    let result2 = 0;
+    return result2;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,67 @@
-mod tools;
 mod day01;
+mod tools;
+
+use chrono::Datelike;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// day to solve
+    #[arg(short = 'd', long)]
+    day: Option<u8>,
+
+    /// solve all days
+    #[arg(long)]
+    all: bool,
+}
+
 fn main() {
     println!("Hello, Advent of Code 2025!");
+
+    let args = Args::parse();
+    // dbg!(args);
+
+    if args.all {
+        for day in 1..=12 {
+            print!("Running day {day}...\n");
+            run_day(day);
+            print!("\n");
+        }
+    } else {
+        let day: u8;
+        if let Some(required_day) = args.day {
+            day = required_day;
+        } else {
+            day = get_current_day();
+        }
+
+        if day > 0 && day <= 12 {
+            print!("Running current day {day}...\n");
+            run_day(day);
+        } else {
+            println!("No day specified and no current day available.");
+        }
+    }
+}
+
+fn get_current_day() -> u8 {
+    let local = chrono::Local::now();
+    let date = local.naive_local();
+    if date.year() == 2025 && date.month() == 12 {
+        return date.day() as u8;
+    }
+    0
+}
+
+fn run_day(day: u8) {
+    match day {
+        1 => solve_day01(),
+        _ => println!("Day not implemented yet"),
+    }
+}
+
+fn solve_day01() {
     let res_day01 = day01::solve();
     println!("Day 01, Part 1 solution: {}", res_day01.0);
     println!("Day 01, Part 2 solution: {}", res_day01.1);
